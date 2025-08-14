3a771c10a9f64955a8077b118cb5cc497ad84bad
Juan Penagos <juanndieg2005@gmail.com>
2025-08-13 20:35:09 -0500

    BASE DEL PROYECTO AGRO SABANA

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,42 @@
+// App.jsx (encabezado correcto)
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './Components/Login';
+import Registro from './Components/Registro';
+import Inicio from './Components/Inicio';
+import NotFound from './Components/NotFound';
+import ProtectedRoute from './Components/PrivateRoute';
+import Admin from './Components/Admin';
+
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Navigate to="/userlogin" />} />
+                <Route path="/userlogin" element={<Login />} />
+                <Route path="/Registro" element={<Registro />} />
+                
+                {/* RUTAS PARA EL ADMINISTRADOR */}
+                <Route path="/Admin" element={
+                    <ProtectedRoute allowedRoles={['ADMIN']}>
+                        <Admin />
+                    </ProtectedRoute>
+                } />
+
+                {/* RUTAS PARA LOS USUARIOS */}   
+
+                <Route path="/Inicio" element={
+                    <ProtectedRoute allowedRoles={['USER']}>
+                        <Inicio />
+                    </ProtectedRoute>
+                } />
+                
+
+                {/* RUTA NO ENCONTRADA */}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
